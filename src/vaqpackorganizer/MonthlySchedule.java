9a5db613651f1cde79d9583396108fd3cab52011
RobertoAlvarez <roberto.a.alvarez01@utrgv.edu>
2016-04-24 16:55:55 -0500

    mostly complete, added a css sheet, just need to store events in a date

--- src/vaqpackorganizer/MonthlySchedule.java
+++ src/vaqpackorganizer/MonthlySchedule.java
@@ -3,14 +3,17 @@ package vaqpackorganizer;
 
 import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -21,8 +24,8 @@ public class MonthlySchedule extends Application{
         
         try {
             HBox rootPane = new HBox(2);
-            Scene scene = new Scene(rootPane, 1080, 720);
             VBox TextFields = new VBox();
+            Scene scene = new Scene(rootPane, 1080, 720);
             
             String css = MonthlySchedule.class.getResource("CalendarStyle.css").toExternalForm();
             scene.getStylesheets().add(css);
@@ -48,8 +51,13 @@ public class MonthlySchedule extends Application{
             TextField eventPlace = new TextField();
             //end of labels and text fields
             
+            //choice box for reminder
+            Label reminderLabel = new Label("Would you like to set a reminder?");
+            ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList("Yes", "No"));
+            cb.setTooltip(new Tooltip("Select Yes or No"));
+            
             //add textfields and labels to TextFields Pane
-            TextFields.getChildren().addAll(nameLabel, eventName, timeLabel, eventTime, placeLabel, eventPlace);
+            TextFields.getChildren().addAll(nameLabel, eventName, timeLabel, eventTime, placeLabel, eventPlace,reminderLabel, cb, eventBtn);
             
             //dimensions for rootPane
             rootPane.setMinWidth(400);
@@ -59,7 +67,7 @@ public class MonthlySchedule extends Application{
             //end
             
             //add calendar, button and textfields pane
-            rootPane.getChildren().addAll(popupContent, eventBtn, TextFields);
+            rootPane.getChildren().addAll(popupContent, TextFields);
             
            
 
