7eb2f45623d018efdfe7f2432d7d97456d25bd7a
RobertoAlvarez <roberto.a.alvarez01@utrgv.edu>
2016-04-24 19:23:25 -0500

    changed methods, and created tab

--- src/vaqpackorganizer/MonthlySchedule.java
+++ src/vaqpackorganizer/MonthlySchedule.java
@@ -2,9 +2,10 @@ package vaqpackorganizer;
 
 
 import com.sun.javafx.scene.control.skin.DatePickerSkin;
-import javafx.application.Application;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javafx.collections.FXCollections;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -12,18 +13,34 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class MonthlySchedule extends Application{
+public class MonthlySchedule {
     
-       @Override
-    public void start(Stage monthlyStage) {
-        
-        try {
-            HBox rootPane = new HBox(2);
+    private Connection conn;
+    private Tab tab;
+    HBox rootPane = new HBox(2);
+    VBox TextFields = new VBox();
+    
+    
+    public void setCalendarTab() {
+        tab = new Tab();
+        tab.setText("Calendar");
+        setCalendar();
+        tab.setContent(rootPane);
+    } 
+    
+    public void setConnection(Connection conn){
+        this.conn = conn;
+    }
+    
+    public void setCalendar() {
+            
+            
             //dimensions for rootPane
             rootPane.setMinWidth(400);
             rootPane.setMaxWidth(400);
@@ -31,7 +48,7 @@ public class MonthlySchedule extends Application{
             rootPane.setMaxHeight(400);
             //end
             
-            VBox TextFields = new VBox();
+            
             Scene scene = new Scene(rootPane, 1080, 720);
             
             String css = MonthlySchedule.class.getResource("CalendarStyle.css").toExternalForm();
@@ -69,14 +86,6 @@ public class MonthlySchedule extends Application{
             
             //add calendar, button and textfields pane
             rootPane.getChildren().addAll(popupContent, TextFields);
-            
-            monthlyStage.setScene(scene);
-            monthlyStage.setTitle("Monthly Calendar");
-            monthlyStage.show();
-            
-        }catch(Exception monthlySchedule) {
-            monthlySchedule.printStackTrace();
-        }
-      
+    
     }
 }
