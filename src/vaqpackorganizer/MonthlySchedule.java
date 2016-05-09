98b8806db5d41971c6aa694cb3f5ea864c0f0c79
RobertoAlvarez <roberto.a.alvarez01@utrgv.edu>
2016-05-09 16:27:12 -0500

    will try to modify choice box

--- src/vaqpackorganizer/MonthlySchedule.java
+++ src/vaqpackorganizer/MonthlySchedule.java
@@ -33,6 +33,11 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+/**
+ *
+ * @author Roberto
+ */
+
 public class MonthlySchedule {
     
     
